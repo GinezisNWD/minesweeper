58c3199e8f4c1f2094ddff53450ec5c272b3bb20
Ginezis <Ginezis25@gmail.com>
2023-05-24 05:08:52 +0300

    fix: fix path for lose.mp3

--- js/render-end-game-message.js
+++ js/render-end-game-message.js
@@ -20,7 +20,7 @@ function renderEndGameMessage(res, time, steps) {
 		popUpMessage.innerText = `Жаль, но вы проиграли за ${steps} шагов и ${time} секунд.\nСыграем еще раз?`
 
 		if (document.querySelector('.minesweeper__sound-chebox').classList.contains('_active')) {
-			const loseSound = new Audio('.../assets/audio/lose.mp3')
+			const loseSound = new Audio('assets/audio/lose.mp3')
 			loseSound.play()
 		}
 	} else {
